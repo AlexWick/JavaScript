86adc79b3d05211da6a3abfd03c541c9b774c510
Alex <alexwick@gmail.com>
2018-11-20 10:46:25 -0500

    Small .js programs
    Beginner stuff

---
+++ assertionTest.js
@@ -0,0 +1,16 @@
+function assertEqual(actual, expected, testName) {
+   if (actual === expected) {
+     console.log('passed');
+   } else {
+     console.log('FAILED ' + testName + ': Expected ' + expected + ', but got ' + actual);
+   }
+}
+object = {a:1,b:2,c:3,D:4,e:5,f:600}
+
+function findAvg(object) {
+    for (var i = 0; i < object.length; i++) {
+      total = valueOf(i){
+        object.Math.sum(key)
+      }
+
+}
---
+++ jScript.js
@@ -0,0 +1,5 @@
+var Object = 'html-5';
+function transformObject(obj);
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array [-1]
+  }
